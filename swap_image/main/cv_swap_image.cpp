52ad878ce4c380deb00c603336c9af089fe0b81e
nopnop2002 <>
2026-06-08 14:22:38 +0900

    typo

--- swap_image/main/cv_swap_image.cpp
+++ swap_image/main/cv_swap_image.cpp
@@ -33,7 +33,7 @@
 using namespace std;
 using namespace cv;
 
-extern "C" void cv_swap_image(uint8_t* data, int dlen, int width, int heigth, int swp_size, int swap_position);
+extern "C" void cv_swap_image(uint8_t* data, int dlen, int width, int heigth, int swap_size, int swap_position);
 
 extern "C" void cv_print_info();
 void cv_print_info()
@@ -41,7 +41,7 @@ void cv_print_info()
 	printf("OpenCV info print: %s", cv::getBuildInformation().c_str());
 }
 
-void cv_swap_image(uint8_t* data, int dlen, int width, int heigth, int swp_size, int swap_position)
+void cv_swap_image(uint8_t* data, int dlen, int width, int heigth, int swap_size, int swap_position)
 {
 	printf("OpenCV swap image.\n");
 	cv::Mat src( heigth, width, CV_16UC1, (void*)data );
@@ -54,26 +54,26 @@ void cv_swap_image(uint8_t* data, int dlen, int width, int heigth, int swp_size,
 
 	int swap_x1 = 0;
 	int swap_y1 = 0;
-	int swap_x2 = src.cols - swp_size;
+	int swap_x2 = src.cols - swap_size;
 	int swap_y2 = swap_y1;
 
 	// Swap position(Top left X coordinate, Y coordinate)
 	if (swap_position == 1) {
-		swap_y1 = (rows / 2) - (swp_size / 2);
+		swap_y1 = (rows / 2) - (swap_size / 2);
 		swap_y2 = swap_y1;
 	} else if (swap_position == 2) {
-		swap_y1 = rows - swp_size;
+		swap_y1 = rows - swap_size;
 		swap_y2 = swap_y1;
 	} else if (swap_position == 3) {
-		swap_x1 = (cols / 2) - (swp_size / 2);
+		swap_x1 = (cols / 2) - (swap_size / 2);
 		swap_x2 = swap_x1;
-		swap_y2 = rows - swp_size;
+		swap_y2 = rows - swap_size;
 	}
 
 	// Crop image
-	cv::Rect roi1 = cv::Rect(swap_x1, swap_y1, swp_size, swp_size); 
+	cv::Rect roi1 = cv::Rect(swap_x1, swap_y1, swap_size, swap_size); 
 	cv::Mat image_roi1 = cloned(roi1);
-	cv::Rect roi2 = cv::Rect(swap_x2, swap_y2, swp_size, swp_size); 
+	cv::Rect roi2 = cv::Rect(swap_x2, swap_y2, swap_size, swap_size); 
 	cv::Mat image_roi2 = cloned(roi2);
 	cv::Mat image_roi3 = src(roi2);
 
